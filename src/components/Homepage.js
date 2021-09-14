7101258872f4a0862f9b901ac9aed197d39e1c48
vanbaotran <vanbaotran201194@gmail.com>
2021-09-14 23:16:49 +0200

    css updates

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,8 +3,10 @@ import Portfolio from './Portfolio'
 
 const Homepage = () => {
     return (
+        <>
         <main className='homepage'>
         <div className='header'>
+        <img src='https://res.cloudinary.com/vanbaotran/image/upload/v1631464401/newlandpage_ixw6qm.png' alt='landpage'/>
             <div className='text'>
             <p>Hello, I'm</p>
             <h2>Bao-Tran Van</h2>
@@ -17,8 +19,9 @@ const Homepage = () => {
         <span className='text-bold'> Portfolio</span>
         </div>
         </div>
-        <Portfolio />
         </main>
+        <Portfolio />
+        </>
     )
 }
 
